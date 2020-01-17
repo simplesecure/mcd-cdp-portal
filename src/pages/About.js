3efe822985002f0a0b3f15c9d4425d9234c52071
Fernando Tiberti <fertiberti@gmail.com>
2020-01-17 09:35:58 -0300

    Updating style.

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,38 +10,86 @@ import SEO from '../components/SEO';
 import mixpanel from 'mixpanel-browser';
 import { Routes } from '../utils/constants';
 
-import {
-  Box
-} from '@makerdao/ui-components-core';
+import { Box } from '@makerdao/ui-components-core';
 import useLanguage from 'hooks/useLanguage';
 
-const TopBackground = styled.div`
+const TopBackground = styled(Box)`
   position: absolute;
   height: 570px;
   top: 0;
   left: 0;
   right: 0;
   z-index: -1;
-  background: ${props => props.background}
+  background: ${props => props.background};
+`;
+
+const Heading = styled(Box)`
+  text-align: center;
+  letter-spacing: 0.4px;
+
+  h1 {
+    font-size: 48px;
+    line-height: 58px;
+
+    letter-spacing: 0.4px;
+
+    color: #253a44;
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 27px;
+    color: #14303a;
+  }
+
+  .button {
+    padding-right: 22px;
+    padding-left: 22px;
+    border-radius: 6px;
+    display: inline-block;
+    font-size: 15px;
+    font-weight: 500;
+    height: 39px;
+    line-height: 38px;
+    text-decoration: none;
+    box-shadow: 0 2px 2px ${props => props.btnShadowColor};
+    transition: all 0.15s ease;
+  }
+
+  .button:hover {
+    box-shadow: 0 5px 5px ${props => props.btnShadowColor};
+    transform: translateY(-1px);
+  }
 `;
 
 const AboutTrade = hot(() => {
+  const { lang } = useLanguage();
 
   return (
-    <OasisLayout style={{position: 'static'}}>
-      <TopBackground background="linear-gradient(180deg, rgba(199, 216, 227, 0.53) 0%, rgba(199, 216, 227, 0.53) 0%, rgba(222, 239, 236, 0.37) 63.02%, rgba(222, 239, 236, 0) 100%)"/>
+    <OasisLayout style={{ position: 'static' }}>
       <SEO title="Oasis - About: Trade" />
-      <h1>Trade</h1>
+      <TopBackground background="linear-gradient(180deg, rgba(199, 216, 227, 0.53) 0%, rgba(199, 216, 227, 0.53) 0%, rgba(222, 239, 236, 0.37) 63.02%, rgba(222, 239, 236, 0) 100%)" />
+      <Heading pColor="#14303A" btnShadowColor="#C8E4E6" btnTextColor="#5894B5">
+        <h1>{lang.landing_page.trade_card.title}</h1>
+        <p>{lang.landing_page.trade_card.description}</p>
+        <a className="button" href={`/${Routes.TRADE}`}>
+          {lang.landing_page.trade_card.button}
+        </a>
+      </Heading>
     </OasisLayout>
   );
 });
 
-const AboutBorrow = () => <OasisLayout>
-  <h1>Borrow</h1>
-</OasisLayout>;
+const AboutBorrow = () => (
+  <OasisLayout>
+    <h1>Borrow</h1>
+  </OasisLayout>
+);
 
-const AboutSave = hot(() => <OasisLayout>
-  <h1>Save</h1>
-</OasisLayout>);
+const AboutSave = hot(() => (
+  <OasisLayout>
+    <h1>Save</h1>
+  </OasisLayout>
+));
 
 export { AboutTrade, AboutBorrow, AboutSave };
